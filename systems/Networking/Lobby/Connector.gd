4cd2a8b730a17e2a3191047b3974de6cf549cdeb
Mastermori <mastermoriii@gmail.com>
2019-11-28 16:34:55 +0100

    Added InfoLabel to lobby

--- systems/Networking/Lobby/Connector.gd
+++ systems/Networking/Lobby/Connector.gd
@@ -2,8 +2,12 @@ extends Control
 
 
 func _on_HostButton_pressed():
-	Network.host(Player.new($"Inputs/NickInput".text, $"Inputs/ColorPicker".color), 6000, 2)
-	get_tree().change_scene("res://levels/Lobby.tscn")
+	var nick = $"Inputs/NickInput".text
+	if nick != "":
+		Network.host(Player.new(nick, $"Inputs/ColorPicker".color), 6000, 2)
+		get_tree().change_scene("res://levels/Lobby.tscn")
+	else:
+		$InfoLabel.show_info("Please enter a nickname")
 
 
 func _on_ConnectButton_pressed():
